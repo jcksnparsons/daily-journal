13a741f946596d9de38c665fc2f95e2a12108603
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-17 15:21:11 -0600

    changed journalDate to proper formatting

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,9 +1,5 @@
 const journalEntry1 = {
-    journalDate: {
-        month: 1,
-        day: 17,
-        year: 2020
-    },
+    journalDate: "2020-01-17",
     conceptsCovered: "Functions in Javascript",
     journalEntry: "My brain feels like a large bowl of mashed potatoes.",
     moodForDay: "flustered",
@@ -17,22 +13,14 @@ function addJournalEntry(journalEntry) {
 }
 
 const journalEntry2 = {
-    journalDate: {
-        month: 1,
-        day: 16,
-        year: 2020
-    },
+    journalDate: "2020-01-16",
     conceptsCovered: "Objects in javascript",
     journalEntry: "Actually not too bad. Objects seem a lot less scary after today.",
     moodForDay: "notSoBad",
 }
 
 const journalEntry3 = {
-    journalDate: {
-        month: 1,
-        day: 16,
-        year: 2020
-    },
+    journalDate: "2020-01-15",
     conceptsCovered: "jQuery and manipulating the DOM",
     journalEntry: "Wow, the DOM now seems way less terrifying. Glad I'm starting to pick things up!",
     moodForDay: "prettyGood",
