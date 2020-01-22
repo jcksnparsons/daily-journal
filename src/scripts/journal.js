ede88302a4bfbff09c4dbd28acb2aa75b93672d8
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-22 15:04:43 -0600

    moved journal entries to local API, rebuilt functions to utilized fetch from API, cleaned up code, changed border on journal entries to orange

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,41 +1,4 @@
-const journalEntry1 = {
-    journalDate: "2020-01-17",
-    conceptsCovered: "Functions in Javascript",
-    journalContent: "My brain feels like a large bowl of mashed potatoes.",
-    moodForDay: "flustered",
-}
-
-let journalCollection = []
-
-function addJournalEntry(journalEntry) {
-    journalCollection.push(journalEntry)
-}
-
-const journalEntry2 = {
-    journalDate: "2020-01-16",
-    conceptsCovered: "Objects in javascript",
-    journalContent: "Actually not too bad. Objects seem a lot less scary after today.",
-    moodForDay: "notSoBad",
-}
-
-const journalEntry3 = {
-    journalDate: "2020-01-15",
-    conceptsCovered: "jQuery and manipulating the DOM",
-    journalContent: "Wow, the DOM now seems way less terrifying. Glad I'm starting to pick things up!",
-    moodForDay: "prettyGood",
-}
-
-const journalEntry4 = {
-    journalDate: "2020-01-20",
-    conceptsCovered: "mainpulating HTML with javascript functions",
-    journalContent: "Today was a solid 50/50. I felt pretty good about things half the time, but also felt like I was looking at hieroglyphics at points. At least I'm not the only one at that point.",
-    moodForDay: "notSoBad",
-}
-
-addJournalEntry(journalEntry1);
-addJournalEntry(journalEntry2);
-addJournalEntry(journalEntry3);
-addJournalEntry(journalEntry4);
+const entryLog = document.getElementById("entryLog")
 
 const makeJournalEntry = (journalEntry) => {
     return `<div class="journalEntry">
@@ -47,12 +10,11 @@ const makeJournalEntry = (journalEntry) => {
     `
 }
 
-const entryLog = document.getElementById("entryLog")
-
-const renderJournalEntries = (journalCollection) => {
-    for (i = 0; i < journalCollection.length; i++) {
-        entryLog.innerHTML += makeJournalEntry(journalCollection[i])
-    }
-}
-
-renderJournalEntries(journalCollection)
+fetch("http://localhost:3000/entries") // Fetch from the API
+    .then(resp => resp.json())  // Parse as JSON
+    .then(entries => {
+        entries.forEach(entry => {
+            entryLog.innerHTML += makeJournalEntry(entry)
+        })
+        // What should happen when we finally have the array?
+    })
