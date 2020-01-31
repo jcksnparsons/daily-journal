e4d308276c1f48f04f997bfa3995888f4a4385ff
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-31 15:27:07 -0600

    got event listener to work and collected data from input fields

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,3 +2,39 @@ import API from './data.js'
 import putJournalOnDOM from './entriesDOM.js'
 
 API.getJournalEntries().then(putJournalOnDOM)
+
+const recordEntryButton = document.querySelector("#recordEntryButton")
+const journalDate = document.querySelector("#journalDate")
+const conceptsCovered = document.querySelector("#conceptsCoveredBox")
+const journalContent = document.querySelector("#journalContent")
+const moodForDay = document.getElementsByName("mood")
+
+const moodChecker = (moodForDay) => {
+    for (let i = 0; i < moodForDay.length; i++) {
+        if (moodForDay[i].checked === true) {
+            return moodForDay[i].value;
+        }
+    }
+}
+
+const journalEntryMaker = (date, concepts, content, mood) => {
+    return {
+        "journalDate": date,
+        "conceptsCovered": concepts,
+        "journalContent": content,
+        "moodForDay": mood
+    }
+}
+
+recordEntryButton.addEventListener('click', event => {
+    const newJournalEntry = journalEntryMaker(journalDate.value, conceptsCovered.value, journalContent.value, moodChecker(moodForDay))
+    fetch("http://localhost:3000/entries", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newJournalEntry)
+    })
+})
+
+
